4cefa58e15087a31065744ea44e4405c8f7f015a
Kim Mok <kim.mok@rakosell.com>
2023-11-16 19:40:12 +0800

    Added uuid and file name filter

--- src/component/DropzoneUploader.js
+++ src/component/DropzoneUploader.js
@@ -1,23 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Dropzone from 'react-dropzone-uploader';
 import 'react-dropzone-uploader/dist/styles.css'
 import { presignedPut } from '../service/s3';
-
-// file path and file name to be uploaded to the s3 bucket
-const getBannerFilename = (username) => {
-    return `assets/${username}/banner.png`;
-};
-
-const getS3UploadUrl = async () => {
-    return await presignedPut(getBannerFilename('s3-demo'));
-};
+import { v4 as uuidv4 } from 'uuid';
 
 const Layout = ({ input, previews, submitButton, dropzoneProps, files, extra: { maxFiles } }) => {
     return (
         <div style={{ margin: 0, padding: 0 }}>
             {previews}
 
-            {previews?.length == 0 && <div {...dropzoneProps}>{files.length < maxFiles && input}</div>}
+            {previews?.length === 0 && <div {...dropzoneProps}>{files.length < maxFiles && input}</div>}
 
             {files.length > 0 && submitButton}
         </div>
@@ -25,9 +17,12 @@ const Layout = ({ input, previews, submitButton, dropzoneProps, files, extra: { 
 };
 
 const DropzoneUploader = (props) => {
+    const [path, setPath] = useState(null);
+    const [imageUrl, setImageUrl] = useState();
+
     const getUploadParams = async ({ file, meta }) => ({
         method: 'PUT',
-        url: await getS3UploadUrl(),
+        url: await getS3UploadUrl(file.name),
         body: file
     });
 
@@ -39,12 +34,36 @@ const DropzoneUploader = (props) => {
             case 'getting_upload_params':
                 break;
             case 'done':
-                alert(`File uploaded to ${process.env.REACT_APP_S3_ENDPOINT}/${process.env.REACT_APP_S3_BUCKET}/${getBannerFilename('s3-demo')}, please reload`);
+                alert(`File uploaded to ${process.env.REACT_APP_S3_ENDPOINT}/${process.env.REACT_APP_S3_BUCKET}/${path}`);
                 break;
             default:
                 break;
         }
     };
+
+    const handleFilename = (filename) => {
+        const regex = /[^a-zA-Z0-9_\-()+&*^\"\'\[\]]/g;
+        return filename.replace(regex, "_");
+    };
+
+    // file path and file name to be uploaded to the s3 bucket
+    const handledPath = (filename) => {
+        const handledFilename = handleFilename(filename);
+        const uuid = uuidv4();
+        const path = `${uuid}/${handledFilename}`
+        setPath(path);
+        return path;
+    };
+
+    const getS3UploadUrl = async (filename) => {
+        return await presignedPut(handledPath(filename));
+    };
+
+    const displayImage = (e) => {
+        const { value } = e.target;
+        // const url = `${process.env.REACT_APP_S3_ENDPOINT}/${process.env.REACT_APP_S3_BUCKET}/${value}`;
+        setImageUrl(value);
+    }
 
     return (
         <>
@@ -53,15 +72,18 @@ const DropzoneUploader = (props) => {
                 getUploadParams={getUploadParams}
                 // onSubmit={handleSubmit}
                 onChangeStatus={handleChange}
-                submitButtonContent={null}
+                // submitButtonContent={null}
                 maxFiles={1}
-                submitButtonDisabled={true}
+                // submitButtonDisabled={true}
                 multiple={false}
                 LayoutComponent={Layout}
                 accept="image/*"
                 styles={{ dropzone: { minHeight: 120 } }} />
-            <div style={{ marginBottom: 5, minHeight: 200, background: `no-repeat 50% 50%/cover url("${process.env.REACT_APP_S3_ENDPOINT}/${process.env.REACT_APP_S3_BUCKET}/${getBannerFilename('s3-demo')}")` }}>
-            </div>
+            <input type='text' name='url' onChange={displayImage} style={{width:'100%'}}/>
+            {imageUrl && (
+                <div style={{ marginBottom: 5, minHeight: 200, background: `no-repeat 50% 50%/cover url("${imageUrl}"` }}>
+                </div>
+            )}
         </>
     )
 }
